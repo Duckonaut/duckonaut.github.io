c101bbb742b21e39eb1e4ff767dc08631a6b67b1
Duckonaut <duckonaut@gmail.com>
2022-03-08 23:07:23 +0100

    new site

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    $('.project-image-gallery').slick();
+	$('.project-image-gallery').slick({
+	});
 });
+
+//$( ".project-image-gallery-item" ).each(function( index ) { 
+//   $( this ).css('margin-top', (360 - $(this).height()) / 2 + 'px' );
+// });
